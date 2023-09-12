315d8cfa9fbf850372522f65c896a898d7f5adf9
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2023-09-12 19:04:20 +0530

    Day1_Practice

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+class Welcome
+{
+
+public static void main(String args[])
+{
+	
+	System.out.println("welcom to java");
+	
+}
+
+}//.java --->.class  compiler-->compailation
+//jvm--output
+				//compiler
+//javac filename.java--->.class --->JVM--->java classname--->output
